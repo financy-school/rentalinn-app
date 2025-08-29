2372950a2371f43ead4af99eec429df06e96fdc8
sumittguptaa <sumitgupta4535@gmail.com>
2025-08-29 23:13:11 +0530

    fix: Update FINANCY_ENDPOINT_URL to use the correct production endpoint

--- config.js
+++ config.js
@@ -1,8 +1,3 @@
-// export const FINANCY_ENDPOINT_URL = 'http://localhost:3000';
+// export const FINANCY_ENDPOINT_URL = 'http://192.168.1.63:4200';
 
-// export const FINANCY_ENDPOINT_URL = 'http://192.168.0.100:3000';
-
-// export const FINANCY_ENDPOINT_URL =
-//   'https://ea44-2406-7400-56-12f8-b55c-8092-fc1f-77a5.ngrok-free.app';
-
-export const FINANCY_ENDPOINT_URL = 'https://rentalinn.zapto.org';
+export const FINANCY_ENDPOINT_URL = 'https://rentalinn.ddns.net';
